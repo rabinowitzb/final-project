f0c5c4ceaf7d538a1f5307bad3a41d7dcea87e42
gabapoj <43244041+gabapoj@users.noreply.github.com>
2018-12-06 18:19:36 -0500

    Update application.py

--- application.py
+++ application.py
@@ -13,7 +13,7 @@ from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions
 from werkzeug.security import check_password_hash, generate_password_hash
-
+ 
 from helpers import apology, login_required, usd
 
 # Configure application
